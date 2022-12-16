f030979e67f2c8400b94b5d822a66cde1a0f6e9d
ionic-bond <ionicbond3@gmail.com>
2022-12-16 23:09:57 +0800

    add local inference

---
+++ inference.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+import os
+import json
+import math
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+
+import commons
+import utils
+from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from models import SynthesizerTrn
+from text.symbols import symbols
+from text import text_to_sequence
+
+from scipy.io.wavfile import write
+
+
+def get_text(text, hps):
+    text_norm = text_to_sequence(text, hps.data.text_cleaners)
+    if hps.data.add_blank:
+        text_norm = commons.intersperse(text_norm, 0)
+    text_norm = torch.LongTensor(text_norm)
+    return text_norm
+
+
+hps = utils.get_hparams_from_file("configs/aqua.json")
+
+net_g = SynthesizerTrn(
+    len(symbols),
+    hps.data.filter_length // 2 + 1,
+    hps.train.segment_size // hps.data.hop_length,
+    **hps.model).cuda()
+_ = net_g.eval()
+
+_ = utils.load_checkpoint("G_25000.pth", net_g, None)
+
+stn_tst = get_text("今回のホロマリカ杯ですが、家族の結婚式がちょうど同日被ってしまっていた為不参加という形になりました。今年の大会に向けて1年間コソ練していたので、正直悔しい気持ちでいっぱいですが、家族の晴れ舞台をしっかりと御祝いしてきます！", hps)
+with torch.no_grad():
+    x_tst = stn_tst.cuda().unsqueeze(0)
+    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+write("test.wav", hps.data.sampling_rate, audio)
+
