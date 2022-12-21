20262b092f66072afa2fa152b029e7784134ddfe
ionic-bond <ionicbond3@gmail.com>
2022-12-21 20:27:36 +0800

    add click

--- inference.py
+++ inference.py
@@ -1,20 +1,11 @@
-import matplotlib.pyplot as plt
-
-import os
-import json
-import math
-import torch
-from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import DataLoader
-
 import commons
 import utils
-from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
 from models import SynthesizerTrn
 from text.symbols import symbols
 from text import text_to_sequence
 
+import click
+import torch
 from scipy.io.wavfile import write
 
 
@@ -26,21 +17,32 @@ def get_text(text, hps):
     return text_norm
 
 
-hps = utils.get_hparams_from_file("configs/aqua.json")
 
-net_g = SynthesizerTrn(
-    len(symbols),
-    hps.data.filter_length // 2 + 1,
-    hps.train.segment_size // hps.data.hop_length,
-    **hps.model).cuda()
-_ = net_g.eval()
+@click.group()
+def cli():
+    pass
 
-_ = utils.load_checkpoint("/content/drive/MyDrive/aqua-vits/G_134000.pth", net_g, None)
 
-stn_tst = get_text("あくありうむもコミケでますぞ", hps)
-with torch.no_grad():
-    x_tst = stn_tst.cuda().unsqueeze(0)
-    x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
-    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
-write("test.wav", hps.data.sampling_rate, audio)
+@cli.command()
+@click.option('--text', required=True)
+@click.option('--model_path', required=True)
+@click.option('--config_path', required=True)
+@click.option('--output_path', required=True)
+def run(text, model_path, config_path, output_path):
+    hps = utils.get_hparams_from_file(config_path)
 
+    net_g = SynthesizerTrn(
+        len(symbols),
+        hps.data.filter_length // 2 + 1,
+        hps.train.segment_size // hps.data.hop_length,
+        **hps.model).cuda()
+    _ = net_g.eval()
+
+    _ = utils.load_checkpoint(model_path, net_g, None)
+
+    stn_tst = get_text(text, hps)
+    with torch.no_grad():
+        x_tst = stn_tst.cuda().unsqueeze(0)
+        x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
+        audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    write(output_path, hps.data.sampling_rate, audio)
